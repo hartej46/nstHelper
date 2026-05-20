8fc22910cefac31aa64e7f268f33f866874f0bf7
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-05-20 15:48:25 +0530

    Added question section where data will load for the question

---
+++ frontend/src/components/Question/Question.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Question() {
+  return (
+    <section className=' w-70 bg-gray-900 '>
+        <h1>
+            this will contain the questions
+            
+        </h1>
+    </section>
+  )
+}
+
+export default Question
