ba5e63efc0e4f1bf4039e87798f6fd96e668ef6b
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-05-27 10:18:03 +0530

    Added async function handler

---
+++ backend/src/utlis/asyncHandler.js
@@ -0,0 +1,9 @@
+export const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+    
+        return await fn(req,res,next)
+
+    } catch (error) {
+        throw new Error("Someting went wrong")
+    }
+}
