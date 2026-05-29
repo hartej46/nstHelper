83614a93fde61915ad1ae58d1cfbd8ce4e01ff4f
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-05-29 14:07:54 +0530

    Added health checkup point

---
+++ backend/src/controller/health.controller.js
@@ -0,0 +1,10 @@
+import { asyncHandler}  from "../utlis/asyncHandler.js"; 
+
+export const checkHealth = asyncHandler(async (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Server is running smoothly! 🚀",
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime()
+    });
+});
