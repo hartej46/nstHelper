7eba37c2eb634985a655e44fe945a53d43982097
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-06-03 09:19:42 +0530

    Fixed some typos

--- backend/src/controller/question.controller.js
+++ backend/src/controller/question.controller.js
@@ -92,7 +92,7 @@ const questionDetail = asyncHandler(async (req, res) => {
     
     return  res.status(200).json({
                 success: true,
-                data: matchingQuestions,    
+                data: matchingQuestion,    
             });
 })
 
