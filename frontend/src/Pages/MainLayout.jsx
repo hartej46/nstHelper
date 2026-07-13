d1cc4f26232b0af70d8db235d8e3eb61b3157518
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-07-14 01:35:13 +0530

    fiex margin isssue

--- frontend/src/Pages/MainLayout.jsx
+++ frontend/src/Pages/MainLayout.jsx
@@ -3,13 +3,13 @@ import { Header, SideBarHome, Footer } from '../components/index';
 
 function MainLayout() {
   return (
-    <div className="flex flex-col min-h-screen w-full bg-gray-50">
+    <div className="flex flex-col h-screen w-full bg-white">
       <Header />
 
       <div className="flex flex-1 min-h-0 w-full">
         <SideBarHome />
 
-        <div className="flex-1 flex flex-col min-w-0">
+        <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
           <main className="flex-1 p-6 overflow-y-auto">
             <Outlet />
           </main>
