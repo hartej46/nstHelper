9cd97a4a7e1bdbf8a4c9d088eb5d069eff11a335
hartej46 <hartejsinghsandhu1977@gmail.com>
2026-07-15 19:47:53 +0530

    added exam  model

---
+++ backend/src/models/exam.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const examSchema = new Schema(
+    {
+        examName: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: [String],
+            required: true
+        },
+        suggestion: {
+            type: String,
+        },
+        codingQuestion: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Question" 
+            }
+        ],
+        mcqQuestion: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "MCQ"
+            }
+        ]
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Exam = mongoose.model("Exam", examSchema);
